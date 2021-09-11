4ecaada37321866b50476ec4a47a1db9949cf741
Mick <mickheudre@gmail.com>
2021-09-11 13:37:48 +0200

    ajout page vente de jeux

---
+++ pages/boardgames.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe class="w-screen h-screen" src="">
+    </iframe>
+</template>
+<script>
+export default {
+    layout: 'no',
+    created() {
+    window.location.href = "https://mickheudre.notion.site/Jeux-vendre-fc8ba5bcbc37485ba7191c7975999536";
+  },
+}
+</script>
